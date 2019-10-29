8d9d12e124e2fa4dc9fa48bacb192c6637b3197a
Kalmár Róbert <kalmar.robert@zengo.eu>
2019-10-29 17:50:16 +0100

    Added XsdType trait

--- src/xsdtypes.rs
+++ src/xsdtypes.rs
@@ -2,12 +2,15 @@ use super::{error::NotGroupMemberError, xml::XmlNode};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-pub trait XsdChoice
+pub trait XsdType
 where
-    Self: Sized,
+    Self: Sized
 {
-    fn is_choice_member<T: AsRef<str>>(node_name: T) -> bool;
     fn from_xml_element(xml_node: &XmlNode) -> Result<Self>;
+}
+
+pub trait XsdChoice : XsdType {
+    fn is_choice_member<T: AsRef<str>>(node_name: T) -> bool;
 
     /// Tries to parse an XmlNode as a choice member.
     /// None is returned if the XmlNode is not a member of the choice element (implementors should return a boxed
